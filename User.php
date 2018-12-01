6d30468f21c1d1f13cdf8cac1469bb09c68713ee
Francisco <franciscoigor@gmail.com>
2018-12-01 11:11:43 -0800

    added validation

--- User.php
+++ User.php
@@ -23,6 +23,10 @@ if ($action=="register"){
 
 if ($action=="get"){
     $user=[];
+    if (!validEmail($id)){
+        responseHeader(400,"Bad request");
+        die();
+    }
     if (file_exists("users/$id")){
         $ser=(file_get_contents("users/$id"));
         $user=unserialize(base64_decode($ser));
